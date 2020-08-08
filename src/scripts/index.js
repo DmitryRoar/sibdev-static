4e554ac651491bba229830a95fa383dc67541c49
Dmitry <roarlove1337@gmail.com>
2020-08-08 18:12:08 +0700

    beta version adaptive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,22 @@
 const $ = selector => document.querySelector(selector) 
 
 const rightWrapper = $('.right-wrapper')
+const burgerWrapper = $('.burger-wrapper')
 
 const burgerBtn = $('.burger')
 const leftPanel = $('.left-panel')
+
+const searchTitle = $('.search__title')
 
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active')
     leftPanel.classList.toggle('active')
     rightWrapper.classList.toggle('active')
+    burgerWrapper.classList.toggle('active')
+})
+
+document.addEventListener('scroll', () => {
+    setTimeout(() => {
+        searchTitle.classList.add('hidden')
+    }, 2000)
 })
